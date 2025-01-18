28d0855328cdae70c405d3a9c93f4b50dee0590e
Samer-Ismael <samerismaeelbat2@hotmail.com>
2025-01-18 20:48:03 +0100

    More aggrissive ram cleaning

--- ram.py
+++ ram.py
@@ -14,21 +14,32 @@ def get_ram_metrics():
         "free": round(ram.available / (1024 ** 3), 2),
     }
 
+
+
 def clear_cache_mem():
     
     current_dir = os.path.dirname(os.path.abspath(__file__))
     path = os.path.join(current_dir, 'lib', 'EmptyStandbyList.exe')
     
+    def clear_cache_more_that_one_time():
+        subprocess.run([path, "workingsets"], check=True)
+        time.sleep(1)
+        subprocess.run([path, "standbylist"], check=True)
+    
+    clear_cache_more_that_one_time()
+    time.sleep(3)
+    clear_cache_more_that_one_time()
+    time.sleep(3)
+    clear_cache_more_that_one_time()
+    gc.collect()
+    
+    print("Working sets cleared successfully!")
+
+    print("Standby list cleared successfully!")
+
     
     if os.path.exists(path):
             try:
-                subprocess.run([path, "workingsets"], check=True)
-                print("Working sets cleared successfully!")
-                                
-                time.sleep(1)
-                
-                subprocess.run([path, "standbylist"], check=True)
-                print("Standby list cleared successfully!")
                 
                 print("Memory cleared successfully!")
             except subprocess.CalledProcessError as e:
